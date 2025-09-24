f8641bbcc930d5cbee60e552fa9b218b21a31737
Wilker <neoxextreme@gmail.com>
2025-09-23 20:35:01 -0400

    Melhorias dto

--- src/main/java/com/wilker/usuario_api/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wilker/usuario_api/infrastructure/exception/GlobalExceptionHandler.java
@@ -14,19 +14,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorDTOResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTOResponse(ex.getMessage()));
     }
-
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ErrorDTOResponse> handleConflictException (ConflictException ex){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorDTOResponse(ex.getMessage()));
     }
-
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorDTOResponse> handleUnauthorizedException(UnauthorizedException ex){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorDTOResponse(ex.getMessage()));
     }
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorDTOResponse> hanldeIllegalArgumentException(IllegalArgumentException ex){
+    public ResponseEntity<ErrorDTOResponse> handleIllegalArgumentException(IllegalArgumentException ex){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTOResponse(ex.getMessage()));
     }
-
 }
